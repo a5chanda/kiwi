c5691b88c9d09baca57eb181f05ae667afd14c6b
Ryan Min <ryanmin42@gmail.com>
2018-09-23 04:40:18 -0400

    configure UI for profile, transactions and assets

--- CoolAdmin/js/loadProfile.js
+++ CoolAdmin/js/loadProfile.js
@@ -1,5 +1,17 @@
 $(document).ready(() => {
     let d = new Date();
     var monthNames = ["January", "February", "March", "April", "May","June","July", "August", "September", "October", "November","December"];
-    $('#transaction-title').html($('#transaction-title').html() + monthNames[d.getMonth()] + " " + d.getFullYear());      
+    $('#transaction-title').html($('#transaction-title').html() + monthNames[d.getMonth()] + " " + d.getFullYear());   
+    let urlParams = new URLSearchParams(window.location.search);
+    let userParam = urlParams.get('username');
+    $.ajax({
+        method: 'GET',
+        url: 'ttp://localhost:4000/channels/kiwi-channel/chaincodes/mycc?peer=peer0.org1.kiwi.com&fcn=query&args=%5B%22*' + userParam + '*%22%5D',
+        headers: {
+            'content-type': 'application.json',
+            'Authorization': 'Bearer null'
+        }
+    }).then((res) => {
+        $('#accountBalance').html(res.netWorth);    
+    });
 });
