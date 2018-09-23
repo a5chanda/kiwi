e6031106221a2d4fa402cbca8ef5662c21f32e36
Ryan Min <ryanmin42@gmail.com>
2018-09-23 10:40:18 -0400

    formatting

--- CoolAdmin/js/loadProfile.js
+++ CoolAdmin/js/loadProfile.js
@@ -1,4 +1,6 @@
+const token = "";
 $(document).ready(() => {
+    
     let d = new Date();
     var monthNames = ["January", "February", "March", "April", "May","June","July", "August", "September", "October", "November","December"];
     $('#transaction-title').html($('#transaction-title').html() + monthNames[d.getMonth()] + " " + d.getFullYear());   
@@ -6,12 +8,12 @@ $(document).ready(() => {
     let userParam = urlParams.get('username');
     $.ajax({
         method: 'GET',
-        url: 'ttp://localhost:4000/channels/kiwi-channel/chaincodes/mycc?peer=peer0.org1.kiwi.com&fcn=query&args=%5B%22*' + userParam + '*%22%5D',
+        url: 'ttp://localhost:4200/channels/kiwi-channel/chaincodes/mycc?peer=peer0.org1.kiwi.com&fcn=query&args=%5B%22*' + userParam + '*%22%5D',
         headers: {
             'content-type': 'application.json',
-            'Authorization': 'Bearer null'
+            'Authorization': 'Bearer ' + token
         }
     }).then((res) => {
-        $('#accountBalance').html(res.netWorth);    
+        $('#accountBalance').html(res.value.netWorth);    
     });
 });
