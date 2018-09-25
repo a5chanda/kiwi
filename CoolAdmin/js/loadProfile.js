df9b54eca0d91bd1e2bfbbe0d4520d096f5d0db4
Abhinav Chanda <abhinav.chanda99@gmail.com>
2018-09-24 22:40:07 -0400

    Updated Readme

--- CoolAdmin/js/loadProfile.js
+++ CoolAdmin/js/loadProfile.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1Mzc3NDIwMDUsInVzZXJuYW1lIjoiVXNlck9yZzEiLCJvcmdOYW1lIjoiT3JnMSIsImlhdCI6MTUzNzcwNjAwNX0.j_VlemeawMG79sWiqmdTwLVizLlH9QdQwgsuXLd5OIA"
-=======
-const token = "";
->>>>>>> 745ca5302ea6fd8528b53a500b40290be9ae17eb
 $(document).ready(() => {
     
     let d = new Date();
@@ -15,21 +11,13 @@ $(document).ready(() => {
     console.log(qURL);
     $.ajax({
         method: 'GET',
-<<<<<<< HEAD
         uri: qURL,
-=======
-        url: 'ttp://localhost:4200/channels/kiwi-channel/chaincodes/mycc?peer=peer0.org1.kiwi.com&fcn=query&args=%5B%22*' + userParam + '*%22%5D',
->>>>>>> 745ca5302ea6fd8528b53a500b40290be9ae17eb
         headers: {
             'content-type': 'application.json',
             'Authorization': 'Bearer ' + token
         }
     }).then((res) => {
-<<<<<<< HEAD
         console.log(res);
         $('#accountBalance').html(res.netWorth);    
-=======
-        $('#accountBalance').html(res.value.netWorth);    
->>>>>>> 745ca5302ea6fd8528b53a500b40290be9ae17eb
     });
 });
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# kIwI
+# Kiwi
 
-## About Kiwi
+## [About Kiwi](images/logo.png)
+
+Kiwi is a customer relationship and asset management platform targeted for small businesses and regular consumers built with IBM's Hyperledger Fabric. The platform aims to help small businesses establish and transition themselves on a decentralized, blockchain network and conduct their daily transactions within the network. Moreover, Kiwi enables small-medium sized businesses to easily conduct business to business and business to consumer transactions. For example, they can manage employee payroll, recruit new employees, list current services and products available for purchase, and contact other businsess to inquire about their services. 
+
+Additionally, the customer facing side enables consumers to conduct peer to peer transactions in the efforts to shift to a cashless economy. Regular customers have the ability to apply to job postings, purchase services offerred by current businesses, and the ability to start their own business as well.
+
